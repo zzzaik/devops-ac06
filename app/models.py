9f3dd2784e4222a9f3f1b3748e6294cd06ae8746
Gustavo Zaik Freitas <zzzaik21@gmail.com>
2017-11-01 22:07:39 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -13,4 +13,7 @@ class Curso(models.Model):
 
 class Vestibular(models.Model):
     nome = models.CharField(max_length=200)
+    
+class Candidato(models.Model):
+    nome = models.CharField(max_length=150)
 
